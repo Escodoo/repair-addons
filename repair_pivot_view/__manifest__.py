d05d1bfd1c49fd1fb0d0b637b9f02c98b082eed6
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-05-26 13:43:08 -0300

    [FIX] repair_pivot: change license

--- repair_pivot_view/__manifest__.py
+++ repair_pivot_view/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2020 - TODAY, Escodoo
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     'name': 'Repair Pivot View',
@@ -7,7 +7,7 @@
         This module adds a pivot view for the repair orders""",
     'version': '12.0.1.0.0',
     'category': 'Manufacturing',
-    'license': 'AGPL-3',
+    'license': 'LGPL-3',
     'author': 'Escodoo,Odoo Community Association (OCA)',
     'website': 'https://github.com/oca/manufacture',
     'depends': [
