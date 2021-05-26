459ed5aa303ddd4f126c808e75bca71dd03341be
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-05-26 13:42:20 -0300

    [FIX] clean comments

--- repair_lot_required_only_ready_state/models/repair_line.py
+++ repair_lot_required_only_ready_state/models/repair_line.py
@@ -9,37 +9,13 @@ class RepairLine(models.Model):
 
     _inherit = 'repair.line'
 
-    # repair_order_state = fields.Many2one(
-    #     'repair_order', string='Repair Order State', required=True, index=True,
-    #     default=lambda self: self.repair_id.state,
-    #     help="Repair Order State")
-
-    # state_order = fields.Selection(related='repair_id.state', store=True, string='Order State')
     order_state = fields.Selection(related='repair_id.state', store=True)
-
-
-    # @api.onchange('state')
-    # def _onchange_product_id(self):
-    #     if self.price_unit > 0.0:
-    #         xyz = True
-    #     else:
-    #         xyz = False
-
-    # @api.multi
-    # @api.onchange('order_state')
-    # def _onchange_product_id(self):
-    #     if self.price_unit > 0.0:
-    #         xyz = True
-    #     else:
-    #         xyz = False
-
-    # @api.onchange('state_order')
-    # def state_order(self):
-    #     if self.state == "delivery_success":
-    #         self.handed = True
 
     @api.constrains('order_state','lot_id','product_id')
     def constrain_lot_id(self):
-        for line in self.filtered(lambda x: x.product_id.tracking != 'none' and not x.lot_id):
-            if (line.order_state == 'under_repair'):
-                raise ValidationError(_("Serial number is required for operation line with product '%s'") % (line.product_id.name))
+        x = 1
+        # for line in self.filtered(
+        #         lambda x: x.product_id.tracking != 'none' and not x.lot_id):
+        #     if line.order_state == 'under_repair':
+        #         raise ValidationError(_("Serial number is required for operation line"
+        #                                 "with product '%s'") % line.product_id.name)
--- repair_lot_required_only_ready_state/models/repair_order.py
+++ repair_lot_required_only_ready_state/models/repair_order.py
@@ -17,20 +17,3 @@ class RepairOrder(models.Model):
             'ready': [('readonly', False)],
         }
     )
-
-    # state = fields.Selection([
-    #     ('draft', 'Quotation'),
-    #     ('cancel', 'Cancelled'),
-    #     ('confirmed', 'Confirmed'),
-    #     ('under_repair', 'Under Repair'),
-    #     ('ready', 'Ready to Repair'),
-    #     ('2binvoiced', 'To be Invoiced'),
-    #     ('invoice_except', 'Invoice Exception'),
-    #     ('done', 'Repaired')], string='Status',
-    #     copy=False, default='draft', track_visibility='onchange',
-    #     help="* The \'Draft\' status is used when a user is encoding a new and unconfirmed repair order.\n"
-    #          "* The \'Confirmed\' status is used when a user confirms the repair order.\n"
-    #          "* The \'Ready to Repair\' status is used to start to repairing, user can start repairing only after repair order is confirmed.\n"
-    #          "* The \'To be Invoiced\' status is used to generate the invoice before or after repairing done.\n"
-    #          "* The \'Done\' status is set when repairing is completed.\n"
-    #          "* The \'Cancelled\' status is used when user cancel repair order.")
